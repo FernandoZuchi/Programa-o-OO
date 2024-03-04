320b09233e275d2c3fa6408d47d4d225b47ce33f
FernandoZuchi <nandozuchi07@gmail.com>
2024-03-03 21:52:32 -0300

    Exercício 5 da lista básica JAVA, último do dia...

--- Exercício5-listaBasica/Exercício5-listaBasica/src/App.java
+++ Exercício5-listaBasica/Exercício5-listaBasica/src/App.java
@@ -7,12 +7,17 @@ public class App {
         //perímetro e o tipo de triângulo. Isósceles, Escaleno ou Equilátero
         Scanner le = new Scanner(System.in);
 
-        System.out.println("Digite os três lados do triângulo: ");
+        System.out.println("Digite o lado 1 do triângulo: ");
         double lado1 = le.nextDouble();
+        System.out.println("Digite o lado 2 do triângulo: ");
         double lado2 = le.nextDouble();
+        System.out.println("Digite o lado 3 do triângulo: ");
         double lado3 = le.nextDouble();
+        
         Triangulo t1 = new Triangulo(lado1, lado2, lado3);
-
         System.out.println("Área: " + t1.area());
+        System.out.println("Perímetro: " + t1.perimetro());
+        t1.tipoTriangulo();
+        le.close();
     }
 }
--- Exercício5-listaBasica/Exercício5-listaBasica/src/Triangulo.java
+++ Exercício5-listaBasica/Exercício5-listaBasica/src/Triangulo.java
@@ -1,21 +1,90 @@
 public class Triangulo {
     private double lado1, lado2, lado3;
 
+    // Método construtor
     public Triangulo(double lado1, double lado2, double lado3)
     {
-        this.lado1 = lado1;
-        this.lado2 = lado2;
-        this.lado3 = lado3;
+        setLado1(lado1);
+        setLado2(lado2);
+        setLado3(lado3);
     }
 
+    // Método para verificar o tipo do triângulo
+    public void tipoTriangulo()
+    {
+        if(eEquilatero())
+        {
+            System.out.println("Triângulo equilátero!");
+        }
+        else if (eEscaleno())
+        {
+            System.out.println("Triângulo escaleno!");
+        }
+        else
+        {
+            System.out.println("Triângulo isósceles");
+        }
+    }
+
+    // Função para identificar tipo
+    public boolean eEscaleno ()
+    {
+        return (lado1 != lado2 && lado1 != lado3 && lado2 != lado3);
+    }
+
+    // Função para identificar tipo
+    public boolean eEquilatero ()
+    {
+        return (lado1 == lado2 && lado1 == lado3);
+    }
+
+    // Função para identificar tipo
+    public boolean eIsosceles ()
+    {
+        return ((lado1 == lado2 && lado1 != lado3) || (lado1 != lado2 && lado1 == lado2));
+    }
+
+    // Método para identificar área do triângulo
     public double area()
     {
         double semiperimetro = (lado1 + lado2 + lado3);
         return Math.sqrt(semiperimetro * (semiperimetro - lado1) *  (semiperimetro - lado2) * (semiperimetro * lado3));
     }
 
+    // Método para identificar perímetro do triângulo
     public double perimetro()
     {
         return lado1 + lado2 + lado3;
+    }
+
+    // Métodos de acesso
+    public void setLado1(double novoLado)
+    {
+        this.lado1 = novoLado;
+    }
+
+    public void setLado2(double novoLado)
+    {
+        this.lado2 = novoLado;
+    }
+
+    public void setLado3(double novoLado)
+    {
+        this.lado3 = novoLado;
+    }
+
+    public double getLado1 ()
+    {
+        return this.lado1;
+    }
+
+    public double getLado2 ()
+    {
+        return this.lado1;
+    }
+
+    public double getLado3 ()
+    {
+        return this.lado1;
     }
 }
